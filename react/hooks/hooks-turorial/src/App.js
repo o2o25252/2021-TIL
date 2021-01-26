97e76e746899aafca40de04d398461a4e39ccbda
임희찬 <imhuichan@imhuichan-ui-MacBookPro.local>
2021-01-26 21:22:16 +0900

    hooks

---
+++ react/hooks/hooks-turorial/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Counter from './Counter';
+import Info from './Info'
+
+const App = () => {
+  
+  return <Counter/>
+   
+  
+}
+
+export default App;
